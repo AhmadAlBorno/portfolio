ae96717b478dbce0e9b239c48272552bed43bb1c
AhmadAlBorno <sameerborno.ahmad@gmail.com>
2026-01-24 18:48:19 +0300

    done

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -9,7 +9,7 @@ import Projects from "./components/Projects";
 import Education from "./components/Education";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import "./App.css";
+import "./app.css";
 
 function App() {
   const [vantaEffect, setVantaEffect] = useState(null);
